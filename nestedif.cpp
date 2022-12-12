0642e358bb507b1f3f98bc56c11cd27d625a7330
Netiar <collimow54@gmail.com>
2022-12-12 08:27:27 +0300

    add nested if statements

--- nestedif.cpp
+++ nestedif.cpp
@@ -1,6 +1,38 @@
 #include<iostream>
 using namespace std;
-int()
+int main()
 {
-    
+    int money, partner_age;
+    cout<<"enter money:";
+    cin>>money;
+
+    if(money>=1000)
+    {
+        cout<<"enter partner age:";
+        cin>>partner_age;
+        if (partner_age>21)
+        {
+            cout<<"let's have wine";            
+        }
+        else
+        {
+            cout<<"let's have Tea"; 
+        }
+    }
+    else
+    {
+        if (money>500)
+        {
+            cout<<"let's have whisky";
+        }
+         else
+        {
+            cout<<"let's have coffee"; 
+        }
+
+    }
+
+    cout<<"  ,then we go home"<<endl;
+    return 0; 
+
 } 
