0f64867c55a4de7d73e8d4e05976ea65005f56ae
Netiar <collimow54@gmail.com>
2022-12-15 08:18:27 +0300

    ft: add while loop code logic

--- while_loop.cpp
+++ while_loop.cpp
@@ -2,5 +2,16 @@
 using namespace std;
 int main()
 {
-    
+    int n;
+    cout<<"Enter number greater than 6:";
+    cin>>n;
+
+    while(n<=6)
+    {
+        cout<<"Enter number greater than 6:";
+        cin>>n;        
+    }
+    cout<<"Arigato!";
+    return 0;
+
 }
