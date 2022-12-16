e6bc5df3781bb86757a397445d04a839dd308f86
Netiar <collimow54@gmail.com>
2022-12-16 08:23:07 +0300

    rf:refactor while loop code

--- while_loop.cpp
+++ while_loop.cpp
@@ -3,12 +3,12 @@ using namespace std;
 int main()
 {
     int n;
-    cout<<"Enter number greater than 6:";
+    cout<<"Enter number between 2 and 7:";
     cin>>n;
 
-    while(n<=6)
+    while(n<=2 || n>=7)
     {
-        cout<<"Enter number greater than 6:";
+        cout<<"Enter valid number:";
         cin>>n;        
     }
     cout<<"Arigato!";
