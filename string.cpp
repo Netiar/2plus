850fc143d5747900985024c1f73c2ee053e140e7
Netiar <collimow54@gmail.com>
2022-12-17 08:32:57 +0300

    add string file

---
+++ string.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+     // Declare and initialize string
+    char str[] = "Raindrop";
+  
+    // Print string
+    cout << str;
+  
+    return 0;
+}
