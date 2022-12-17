c3972658a47a0b7388716427711a4eaf05619890
Netiar <collimow54@gmail.com>
2022-12-17 08:38:42 +0300

    ft: add second string

--- string.cpp
+++ string.cpp
@@ -1,12 +1,19 @@
 #include<iostream>
+#include <string>
+
 using namespace std;
 int main()
 {
      // Declare and initialize string
-    char str[] = "Raindrop";
+    string str1 = "Welcome to Raindrops app!";
+
+     // Initialization by raw string
+    string str2("A sexy Manga site.");
   
     // Print string
-    cout << str;
+    cout << str1 << endl << str2;
+  
+   
   
     return 0;
 }
